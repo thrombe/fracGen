d001554d7e06bcc02fb82b607174d250b42efac1
thrombe <yash.n.thombare620@gmail.com>
2021-05-31 11:57:56 +0000

    added mandlebrot, chaos game fractals, buddhabrot

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+    "fmt"
+    "time"
+)
+
+func shhh(vals ...interface{}) {
+    for _, val := range vals {
+        _ = val
+    }
+}
+
+func main() {
+    start := time.Now()
+    defer func() {fmt.Println(time.Now().Sub(start))}()
+    // mandlebrot()
+    // chaosFrac(5, 0.5, 5000000, 10000, 1)
+	// genImg()
+	// polynomialDist()
+    buddhabrot()
+}
+
+func genImg() {
+    width := 1000
+    height := 1000
+    img, set := newImg(width, height)
+    // Set color for each pixel.
+    var t float64 = 1
+    dt := 1/float64(height)
+    for y := 0; y < height; y++ {
+        for x := 0; x < width; x++ {
+            rad := 229*(1-t) + t*148
+            grn := 240*(1-t) + t*191
+            blu := 255*(1-t) + t*255
+            // rad = int(math.Round(mapp(float64(rad))))
+            // grn = int(math.Round(mapp(float64(grn))))
+            // blu = int(math.Round(mapp(float64(blu))))
+            set(x, y, int(rad), int(grn), int(blu))
+        }
+        t -= dt
+    }
+    dumpImg(img)
+}
